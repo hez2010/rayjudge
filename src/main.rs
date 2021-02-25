f7cb1fd425514110411853ec897bf75a8bb1b2d6
Steven He <hez2010@outlook.com>
2021-02-25 21:07:52 +0800

    fix: conditional compile on linux

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,10 @@ use queue::{Queue, QueuePublisher, QueueSubscriber};
 use schema::{JudgeConfig, JudgeResult, Program};
 use std::{convert::TryInto, thread};
 use std_semaphore::Semaphore;
+#[cfg(target_os = "windows")]
 use worker::{windows_worker::WindowsWorker, worker::Worker};
+#[cfg(target_os = "linux")]
+use worker::{linux_worker::LinuxWorker, worker::Worker};
 
 #[macro_use]
 extern crate lazy_static;
